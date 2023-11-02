4a0bae20b9d063fa01ef706a44cb34d1cc35bdf0
senior-talented-dev <webdevstar1997@outlook.com>
2023-11-03 08:19:48 +1100

    js import issue

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,13 +1,41 @@
 import { defineConfig, bytecodePlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   publicDir: false,
   main: {
-    plugins: [bytecodePlugin()],
+    build: { minify: "esbuild" },
+    plugins: [
+      bytecodePlugin({
+        protectedStrings: [],
+      }),
+    ],
   },
-  preload: {},
+  preload: {
+    build: { minify: "esbuild" },
+  },
   renderer: {
+    publicDir: resolve(__dirname, "src", "public"),
+    build: {
+      minify: "esbuild",
+      rollupOptions: {
+        output: {
+          manualChunks: [],
+        },
+      },
+    },
+    server: { hmr: { overlay: false } },
+    esbuild: {
+      loader: "jsx",
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: { ".js": "jsx" },
+      },
+    },
     plugins: [react()],
   },
 });
